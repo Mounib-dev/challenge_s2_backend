f93c480ae6435651974dea6009fb6f0e0612638d
M. Mounib OUROUA <ourouamounib@gmail.com>
2024-05-28 20:18:25 +0200

    Adding a special method to the Team Members Model for hashing passwords at every new record

--- models/teamMemberModel.js
+++ models/teamMemberModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const teamMemberSchema = new mongoose.Schema({
   firstname: String,
@@ -7,6 +8,15 @@ const teamMemberSchema = new mongoose.Schema({
   email: { type: String, unique: true },
   password: String,
   tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Tasks" }],
+});
+
+teamMemberSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 const TeamMember = mongoose.model("TeamMember", teamMemberSchema);
