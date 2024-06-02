20407a030c1699bbd23b36350ad96124ac057a33
M. Mounib OUROUA <ourouamounib@gmail.com>
2024-06-02 15:05:24 +0200

    Adding tests configuration and upgrading the database config file

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const dbName = "workloads";
+const dbName = process.env.DB_NAME || "workloads";
 const uri = `mongodb://127.0.0.1:27017/${dbName}`;
 const options = {
   useNewUrlParser: true,
@@ -15,7 +15,23 @@ class Database {
   async _connect() {
     try {
       await mongoose.connect(uri, options);
+      console.log(dbName);
       console.log("Connected to MongoDB workloads Database");
+      if (dbName === "workloads_test") {
+        const teamMembersCollection = await mongoose.connection.db.collection(
+          "teammembers"
+        );
+        await teamMembersCollection.deleteMany({});
+        const tasksCollection = await mongoose.connection.db.collection(
+          "tasks"
+        );
+        await tasksCollection.deleteMany({});
+        const teamsCollection = await mongoose.connection.db.collection(
+          "teams"
+        );
+        await teamsCollection.deleteMany({});
+        console.log("All data deleted from the tests database");
+      }
     } catch (err) {
       console.error("Error connecting to the Database");
     }
