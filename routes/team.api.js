10bb6c8845e97d5a8e0ec1324d8dd1611294cd44
M. Mounib OUROUA <ourouamounib@gmail.com>
2024-06-05 18:27:17 +0200

    Fix: Team API and adding return statements

--- routes/team.api.js
+++ routes/team.api.js
@@ -7,43 +7,55 @@ const TeamMember = require("../models/teamMemberModel");
 // Route pour obtenir toutes les équipes
 router.get("/", async (req, res, next) => {
   try {
-    const teams = await Team.find({}).populate("members", "firstname lastname -_id");
-    res.status(200).json(teams);
+    const teams = await Team.find({}).populate(
+      "members",
+      "firstname lastname -_id"
+    );
+    return res.status(200).json(teams);
   } catch (err) {
     console.error(err);
-    res.status(500).send("Problème interne du serveur, nous nous excusons pour la gêne occasionnée");
+    return res
+      .status(500)
+      .send(
+        "Problème interne du serveur, nous nous excusons pour la gêne occasionnée"
+      );
   }
 });
 
-// Route pour afficher la liste des équipes 
+// Route pour afficher la liste des équipes
 router.get("/teams", async (req, res, next) => {
   try {
-    const teams = await Team.find({ type: { $exists: false } }).populate("members", "firstname lastname -_id");
+    const teams = await Team.find({ type: { $exists: false } }).populate(
+      "members",
+      "firstname lastname -_id"
+    );
     if (teams.length > 0) {
       return res.status(200).json(teams);
     }
     return res.status(400).json({ message: "No teams found" });
   } catch (err) {
     console.error(err);
-    res.status(500).send("Sorry, something went wrong with the server");
+    return res.status(500).send("Sorry, something went wrong with the server");
   }
 });
 // Route pour obtenir une équipe par son ID
 router.get("/:id", async (req, res, next) => {
   try {
     const teamId = req.params.id;
-    const team = await Team.findById(teamId).populate("members", "firstname lastname -_id");
+    const team = await Team.findById(teamId).populate(
+      "members",
+      "firstname lastname -_id"
+    );
     if (team) {
-      res.status(200).json(team);
+      return res.status(200).json(team);
     } else {
-      res.status(404).send({ message: "Team not found" });
+      return res.status(404).send({ message: "Team not found" });
     }
   } catch (err) {
     console.error(err);
-    res.status(500).send("Sorry, something went wrong with the server");
+    return res.status(500).send("Sorry, something went wrong with the server");
   }
 });
-
 
 // Route pour créer une équipe sans membres
 router.post("/create", async (req, res, next) => {
@@ -54,12 +66,14 @@ router.post("/create", async (req, res, next) => {
       name,
       creationDate,
       description,
-      members: []
+      members: [],
     });
 
     await newTeam.save();
 
-    return res.status(201).json({ message: "Team created successfully", team: newTeam });
+    return res
+      .status(201)
+      .json({ message: "Team created successfully", team: newTeam });
   } catch (err) {
     console.error(err);
     if (err.code == 11000) {
@@ -77,11 +91,10 @@ router.put("/edit/:id", async (req, res, next) => {
     name,
     creationDate,
     description,
-    members 
+    members,
   };
 
   try {
-    
     const result = await Team.updateOne(
       { _id: new ObjectID(id) },
       {
@@ -89,33 +102,31 @@ router.put("/edit/:id", async (req, res, next) => {
       }
     );
 
-    if (result.matchedCount > 0) {
-   
-      await TeamMember.deleteMany({ teamId: new ObjectID(id) });
+    // if (result.matchedCount > 0) {
 
-      
-      if (members && members.length > 0) {
-        const membersPromises = members.map(async memberId => {
-          const teamMember = new TeamMember({
-            teamId: new ObjectID(id),
-            memberId: new ObjectID(memberId)
-          });
-          await teamMember.save();
-        });
-        await Promise.all(membersPromises);
-      }
+    //   await TeamMember.deleteMany({ teamId: new ObjectID(id) });
 
-      res.status(200).send("Team and members edited successfully");
-    } else {
-      res.status(404).send("Team not found");
-    }
+    //   if (members && members.length > 0) {
+    //     const membersPromises = members.map(async memberId => {
+    //       const teamMember = new TeamMember({
+    //         teamId: new ObjectID(id),
+    //         memberId: new ObjectID(memberId)
+    //       });
+    //       await teamMember.save();
+    //     });
+    //     await Promise.all(membersPromises);
+    //   }
+
+    //   res.status(200).send("Team and members edited successfully");
+    // } else {
+    //   res.status(404).send("Team not found");
+    // }
+    return res.status(204).send("Team successefuly edited");
   } catch (err) {
     console.error(err);
-    res.status(500).send("Sorry, something went wrong with the server");
+    return res.status(500).send("Sorry, something went wrong with the server");
   }
 });
-
-
 
 // Route pour supprimer une équipe
 router.delete("/delete/:id", async (req, res, next) => {
@@ -124,13 +135,13 @@ router.delete("/delete/:id", async (req, res, next) => {
   try {
     const result = await Team.deleteOne({ _id: new ObjectID(id) });
     if (result.deletedCount === 1) {
-      res.status(200).send("Team deleted");
+      return res.status(200).send("Team deleted");
     } else {
-      res.status(404).send("Team not found");
+      return res.status(404).send("Team not found");
     }
   } catch (err) {
     console.error(err);
-    res.status(500).send("Sorry, something went wrong with the server");
+    return res.status(500).send("Sorry, something went wrong with the server");
   }
 });
 
