0fc08b4f34e9c34e6e5c4638405ae3424b1a0d44
l3miage-larbiy <larbi1992yasmine@gmail.com>
2024-06-05 20:52:54 +0200

    add task api

--- models/taskModel.js
+++ models/taskModel.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const taskSchema = new mongoose.Schema({
   title: { type: String, unique: true },
-  description: String,
   priority: { type: String, enum: ["Low", "Medium", "High"] },
   deadline: Date,
   assignedTo: [{ type: mongoose.Schema.Types.ObjectId, ref: "TeamMember" }],
--- routes/tasks.api.js
+++ routes/tasks.api.js
@@ -1,19 +1,71 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const Task = require("../models/taskModel");
+const Task = require('../models/taskModel');
 
-router.get("/", async (req, res, next) => {
+// Contrôleur pour créer une nouvelle tâche
+router.post('/', async (req, res) => {
   try {
-    const tasks = await Task.find({});
-    console.log(tasks);
-    res.status(200).json(tasks);
+
+    console.log(req.body)
+    const task = new Task(req.body);
+
+    const newTask = await task.save();
+    return res.status(201).json(newTask);
   } catch (err) {
     console.error(err);
-    res
-      .status(500)
-      .send(
-        "Problème interne du serveur, nous nous excusons pour la gêne occasionnée"
-      );
+    if (err.code === 11000) {
+      return res.status(409).send("Tâche déjà existante");
+    }
+    return res.status(500).json({ message: err.message });
+  }
+});
+
+// Contrôleur pour obtenir toutes les tâches
+router.get('/', async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.json(tasks);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Contrôleur pour obtenir une tâche par son ID
+router.get('/:id', async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    if (!task) {
+      return res.status(404).json({ message: 'Task not found' });
+    }
+    res.json(task);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Contrôleur pour mettre à jour une tâche
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!updatedTask) {
+      return res.status(404).json({ message: 'Task not found' });
+    }
+    res.json(updatedTask);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Contrôleur pour supprimer une tâche
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedTask = await Task.findByIdAndDelete(req.params.id);
+    if (!deletedTask) {
+      return res.status(404).json({ message: 'Task not found' });
+    }
+    res.json({ message: 'Task deleted' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 });
 
