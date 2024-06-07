09a5bb18bd43dcd42d889511ecf699d11738c0d0
M. Mounib OUROUA <ourouamounib@gmail.com>
2024-06-07 11:17:13 +0200

    Minor change: adding missing return statements and status codes for some responses

--- routes/tasks.api.js
+++ routes/tasks.api.js
@@ -1,12 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Task = require('../models/taskModel');
+const Task = require("../models/taskModel");
 
 // Contrôleur pour créer une nouvelle tâche
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   try {
-
-    console.log(req.body)
+    console.log(req.body);
     const task = new Task(req.body);
 
     const newTask = await task.save();
@@ -21,51 +20,53 @@ router.post('/', async (req, res) => {
 });
 
 // Contrôleur pour obtenir toutes les tâches
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const tasks = await Task.find();
-    res.json(tasks);
+    return res.status(200).json(tasks);
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    return res.status(500).json({ message: err.message });
   }
 });
 
 // Contrôleur pour obtenir une tâche par son ID
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
     const task = await Task.findById(req.params.id);
     if (!task) {
-      return res.status(404).json({ message: 'Task not found' });
+      return res.status(404).json({ message: "Task not found" });
     }
-    res.json(task);
+    return res.status(200).json(task);
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    return res.status(500).json({ message: err.message });
   }
 });
 
 // Contrôleur pour mettre à jour une tâche
-router.put('/:id', async (req, res) => {
+router.put("/:id", async (req, res) => {
   try {
-    const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
     if (!updatedTask) {
-      return res.status(404).json({ message: 'Task not found' });
+      return res.status(404).json({ message: "Task not found" });
     }
-    res.json(updatedTask);
+    return res.status(204).json(updatedTask);
   } catch (err) {
-    res.status(400).json({ message: err.message });
+    return res.status(400).json({ message: err.message });
   }
 });
 
 // Contrôleur pour supprimer une tâche
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   try {
     const deletedTask = await Task.findByIdAndDelete(req.params.id);
     if (!deletedTask) {
-      return res.status(404).json({ message: 'Task not found' });
+      return res.status(404).json({ message: "Task not found" });
     }
-    res.json({ message: 'Task deleted' });
+    return res.status(204).json({ message: "Task deleted" });
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    return res.status(500).json({ message: err.message });
   }
 });
 
