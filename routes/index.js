ca9c2baa642ac857f5da812c648eed65d469f525
M. Mounib OUROUA <ourouamounib@gmail.com>
2024-06-19 17:06:43 +0200

    Integrating authentication middleware on every endpoint (except /login)

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 import express from "express";
+import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
 /* API Portal */
-router.get("/", function (req, res, next) {
-  res.send("Welcome");
+router.get("/", auth, function (req, res, next) {
+  res.send("Welcome to API Portal");
 });
 
 export default router;
